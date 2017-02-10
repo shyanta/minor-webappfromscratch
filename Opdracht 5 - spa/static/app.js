ea7d854357b4766823632ff828d37f236fa1b5ee
Camille Sébastien Niessen <mail@camille-sebastien.com>
2017-02-10 13:04:31 +0100

    Code review
    Voila, bij vragen let me know!

--- Opdracht 5 - spa/static/app.js
+++ Opdracht 5 - spa/static/app.js
@@ -1,8 +1,10 @@
 (function(){
+	"use strict"
+	var sections = document.querySelectorAll('section:not(#navigatie):not(#home)'); // Nu hoef je hem niet nog een keer te declareren.
 	var app = {
 		init: function(){
 			routes.init();
-			var sections = document.querySelectorAll('section:not(#navigatie):not(#home)');
+// 			var sections = document.querySelectorAll('section:not(#navigatie):not(#home)');
 
 			sections.forEach(function(sections){
 				sections.hidden = true;
@@ -11,17 +13,23 @@
 	};
 	var routes = {
 		init: function(){
-			window.addEventListener('hashchange', function(){
-				var hash = location.hash;
-				sections.toggle(hash);
-			});
+// 			window.addEventListener('hashchange', function(){
+// 				var hash = location.hash;
+// 				sections.toggle(hash);
+// 			});
+			// Dit kan beter zo
+			window.onhashchange = function() {
+				sections.toggle(); 
+			}
 		}
 	};
 	var sections = {
-		toggle: function(route){
-			var hash = location.hash;
+		toggle: function(){ // Je gebruikt route nergens, dus die heb je ook niet nodig.
+			var hash = window.location.hash; // Met window ervoor is netter
 			var sectionCurrent = document.querySelector('' + hash + '');
-			var sections = document.querySelectorAll('section:not(#navigatie):not('+hash+')');
+// 			var sections = document.querySelectorAll('section:not(#navigatie):not('+hash+')');  <-- Staat bovenin al gedeclareerd
+			
+			// Console.log zou ik weghalen als je klaar bent met testen
 			console.log(sections);
 			console.log(sectionCurrent);
 
