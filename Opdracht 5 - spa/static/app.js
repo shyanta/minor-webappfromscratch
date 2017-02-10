2a5069a1978c2da53231143201e5dc878f6b6175
Shyanta Vleugel <Shyanta Vleugel>
2017-02-10 09:44:58 +0100

    Final Changes for Toggle

--- Opdracht 5 - spa/static/app.js
+++ Opdracht 5 - spa/static/app.js
@@ -2,16 +2,33 @@
 	var app = {
 		init: function(){
 			routes.init();
+			var sections = document.querySelectorAll('section:not(#navigatie)');
+
+			sections.forEach(function(sections){
+				sections.hidden = true;
+			});
 		}
 	};
 	var routes = {
 		init: function(){
-			window.addEventListener('hashchange', sections.toggle());
+			window.addEventListener('hashchange', function(){
+				var hash = location.hash;
+				sections.toggle(hash);
+			});
 		}
 	};
 	var sections = {
 		toggle: function(route){
-			alert("The anchor part has changed!");
+			var hash = location.hash;
+			var sectionCurrent = document.querySelector('' + hash + '');
+			var sections = document.querySelectorAll('section:not(#navigatie):not('+hash+')');
+			console.log(sections);
+			console.log(sectionCurrent);
+
+			sections.forEach(function(sections){
+				sections.hidden = true;
+				sectionCurrent.hidden = false;
+			});
 		}
 	};
 
