1f3d9157c523f55eac4c2b0e1cb252771e47e9c1
Shyanta Vleugel <Shyanta Vleugel>
2017-02-13 10:58:21 +0100

    Applied final Issues Changes

--- Opdracht 5 - spa/static/app.js
+++ Opdracht 5 - spa/static/app.js
@@ -1,20 +1,18 @@
 (function(){
 	"use strict";
 
-	var sectionArr = document.querySelectorAll('section:not(#navigatie):not(#home)');
-		sectionArr.forEach(function(sections){
-			sectionArr.hidden = true;
-		});
-
 	var app = {
 		init: function(){
 			routes.init();
 		}
 	};
 	var routes = {
-		init: function(){
-			window.addEventlistener('hashchange', function(){
-				sections.toggle();
+		init: function(){			
+			window.location.hash = "#home";
+			sections.toggle();
+			window.addEventListener('hashchange', function(){
+				var hash = window.location.hash;
+				sections.toggle(hash);
 			});
 		}
 	};
@@ -23,7 +21,7 @@
 			var hash = window.location.hash;
 			var current = document.querySelector('' + hash + '');
 			var sectionArr = document.querySelectorAll('section:not(#navigatie):not('+hash+')');
-			sectionArr.forEach(function(sections){
+			sectionArr.forEach(function(sectionArr){
 				sectionArr.hidden = true;
 				current.hidden = false;
 			});
