c60db95410ad27864fd6f16e9255863fc45a9799
Shyanta Vleugel <Shyanta Vleugel>
2017-02-14 12:59:26 +0100

    Make API Work

--- Week 2/Opdracht1-API/srv/static/app.js
+++ Week 2/Opdracht1-API/srv/static/app.js
@@ -3,7 +3,7 @@ var search = 'funny+cat';
 
 aja()
 	.method('get')
-	.url('https://api.giphy.com/search?q=' + search +'&api_key='+ API_KEY)
+	.url('https://api.giphy.com/v1/gifs/search?q=' + search +'&api_key='+ API_KEY)
 	.on('200', function(data){
     	console.log(data);
 	})
